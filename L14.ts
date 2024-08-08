47b191b0cd5546f3056d5cc83ea75167e97ff5ec
Shahwaiz Islam <s1404islam@gmail.com>
2024-08-08 18:46:14 +0530

    L14

--- L14.ts
+++ L14.ts
@@ -8,9 +8,28 @@ interface User4 {
   startTrail(): string;
 }
 
-const hitesh: User4 = {
+//u forgot to add github id id above so can do it
+interface User4 {
+  github: string;
+}
+
+const shahwaiz2: User4 = {
   dbId: 22,
   email: "s@s.com",
+  github: "CoderXYZ14",
   userId: 122,
   startTrail: () => "trail started",
+};
+
+interface Admin extends User4 {
+  role: "admin" | "ta" | "lerner";
+}
+
+const shahwaiz3: Admin = {
+  dbId: 22,
+  email: "s@s.com",
+  github: "CoderXYZ14",
+  userId: 122,
+  startTrail: () => "trail started",
+  role: "admin",
 };
