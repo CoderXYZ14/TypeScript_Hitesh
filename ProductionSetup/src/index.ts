bed861544fc16521b0c3dbe979f7dd94ccd8d75e
Shahwaiz Islam <s1404islam@gmail.com>
2024-08-19 14:01:45 +0530

    L17

--- ProductionSetup/src/index.ts
+++ ProductionSetup/src/index.ts
@@ -9,8 +9,29 @@
 // }
 
 class User {
+  private _courseCount = 1;
   private readonly city: string = "Lucknow";
   constructor(public email: string, public name: string) {} //better syntax
+
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+
+  //getter
+  get getAppleEmail(): string {
+    return `apple-${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  //setter shouldn't have a return type
+  set courseCount(courseNum) {
+    if (courseNum <= 1)
+      throw new Error("Course count should be greater than 1");
+    this._courseCount = courseNum;
+  }
 }
 
 const shahwaiz = new User("s@s.com", "shahwaiz");
